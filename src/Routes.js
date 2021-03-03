00d4cd43b2d11b21a5510e424a0423f54c4dbcb5
김보경 <akbk1004@gmail.com>
2021-03-03 13:59:15 +0900

    Modify: 메인페이지 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,20 @@
 import React, {Component} from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-import Nav from "./components/Nav/Nav";
-import Footer from "./components/Footer/Footer";
+// import Nav from "./components/Nav/Nav";
+// import Footer from "./components/Footer/Footer";
 import MainPage from "./pages/Mainpage/Mainpage";
-import LoginBox from "./components/LoginBox/LoginBox";
+//import LoginBox from "./components/LoginBox/LoginBox";
 
 class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/MainPage" component={MainPage} />
-          <Route exact path="/Nav" component={Nav} />
+          <Route exact path="/" component={MainPage} />
+          {/* <Route exact path="/Nav" component={Nav} />
           <Route exact path="/Footer" component={Footer} />
-          <Route exact path="/LoginBox" component={LoginBox} />
+          <Route exact path="/LoginBox" component={LoginBox} /> */}
         </Switch>
       </Router>
     );
